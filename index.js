9daa748495d1d756150bbfe3d9df0d7f1dbd54f1
Matteo Landi <matteo@matteolandi.net>
2018-11-03 16:38:53 +0100

    Lint index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-#!/usr/bin/env node
-
 const lsp = require('vscode-languageserver');
+const createLinter = require('./lint');
 
 const connection = lsp.createConnection();
 const documents = new lsp.TextDocuments();
 const capabilities = {};
 
-connection.onInitialize(params => {
-    module.exports.root = params.rootPath;
-    Object.assign(capabilities, require('./lint')(params.rootPath));
-    return {
-        capabilities: {
-            textDocumentSync: documents.syncKind,
-            completionProvider: {
-                triggerCharacters: ['.'],
-            },
-        },
-    };
+connection.onInitialize((params) => {
+  module.exports.root = params.rootPath;
+  Object.assign(capabilities, createLinter(params.rootPath));
+  require('./analysis');
+  return {
+    capabilities: {
+      textDocumentSync: documents.syncKind,
+      completionProvider: {
+        triggerCharacters: ['.'],
+      },
+    },
+  };
 });
 
-documents.onDidOpen(event => {
+documents.onDidOpen((event) => {
   connection.sendDiagnostics({
     uri: event.document.uri,
     diagnostics: capabilities.getDiagnostics(event),
   });
 });
-documents.onDidChangeContent(event => {
+documents.onDidChangeContent((event) => {
   connection.sendDiagnostics({
     uri: event.document.uri,
     diagnostics: capabilities.getDiagnostics(event),
   });
 });
-documents.onDidClose(event => {
-    connection.sendDiagnostics({uri: event.document.uri, diagnostics: []});
+documents.onDidClose((event) => {
+  connection.sendDiagnostics({ uri: event.document.uri, diagnostics: [] });
 });
 
 documents.listen(connection);
 connection.listen();
 
-module.exports = {connection, documents};
+module.exports = { connection, documents };
